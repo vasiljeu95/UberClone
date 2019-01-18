4bd778c53020cfc694079958d6917f914f45cd19
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-01-18 12:43:35 +0300

    Initial Commit

---
+++ UberClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UberClone
+//
+//  Created by Stepan Vasiljeu on 1/18/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
