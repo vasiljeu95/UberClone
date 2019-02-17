be28a805e61c24de012bac20c699e2a1d14b2e1e
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-02-17 11:32:09 +0300

    v.0.1

--- UberClone/ViewController.swift
+++ UberClone/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLavel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topButton: UIButton!
+    @IBOutlet weak var buttonButton: UIButton!
+    
+    var singUpMode = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func topTapped(_ sender: Any) {
+    }
+    @IBAction func buttonTapped(_ sender: Any) {
+        if singUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            buttonButton.setTitle("Switch to Sing Up", for: .normal)
+        } else {
+            
+        }
+    }
+    
 
 }
 
