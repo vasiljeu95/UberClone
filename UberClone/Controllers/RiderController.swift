6348dd53e908a225b077987370df90d4ac9a241d
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-02-20 22:59:59 +0300

    RiderController.0.4

--- UberClone/Controllers/RiderController.swift
+++ UberClone/Controllers/RiderController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import FirebaseDatabase
 
 class RiderController: UIViewController, CLLocationManagerDelegate {
 
@@ -30,12 +31,17 @@ class RiderController: UIViewController, CLLocationManagerDelegate {
             let center = CLLocationCoordinate2D(latitude: cord.latitude, longitude: cord.longitude)
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             map.setRegion(region, animated: true)
-            
+            map.removeAnnotations(map.annotations)
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = center
+            annotation.title = "Your Location"
+            map.addAnnotation(annotation)
         }
     }
     
     @IBAction func callUberTapped(_ ender: Any) {
-        
+        let rideRequestDictionary: [String:Any] = ["email":"rider@gmail.com", "lat":10, "lon":15]
+        Database.database().reference().child("RideRequests").childByAutoId().setValue(<#T##value: Any?##Any?#>)
     }
     
     @IBAction func logoutTapped(_ ender: Any) {
