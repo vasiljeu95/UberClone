c5e4e79f8dd5288b942f9378d319215f50ef12e4
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-02-21 20:28:40 +0300

    RiderController.0.5

--- UberClone/Controllers/RiderController.swift
+++ UberClone/Controllers/RiderController.swift
@@ -9,26 +9,38 @@
 import UIKit
 import MapKit
 import FirebaseDatabase
+import FirebaseAuth
 
 class RiderController: UIViewController, CLLocationManagerDelegate {
-
+    
     @IBOutlet weak var map: MKMapView!
     @IBOutlet weak var callAnUberButton: UIButton!
     
     var locationManager = CLLocationManager()
+    var userLocation = CLLocationCoordinate2D()
+    var uberHasBeenCalled = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
+        
+        if let email = Auth.auth().currentUser?.email {
+            Database.database().reference().child("RideRequests").childByAutoId().queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapshot) in
+                self.uberHasBeenCalled = true
+                self.callAnUberButton.setTitle("Cancel Uber", for: .normal)
+                Database.database().reference().child("RideRequests").removeAllObservers()
+            }
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         if let cord = manager.location?.coordinate {
             let center = CLLocationCoordinate2D(latitude: cord.latitude, longitude: cord.longitude)
+            userLocation = center
             let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
             map.setRegion(region, animated: true)
             map.removeAnnotations(map.annotations)
@@ -40,12 +52,26 @@ class RiderController: UIViewController, CLLocationManagerDelegate {
     }
     
     @IBAction func callUberTapped(_ ender: Any) {
-        let rideRequestDictionary: [String:Any] = ["email":"rider@gmail.com", "lat":10, "lon":15]
-        Database.database().reference().child("RideRequests").childByAutoId().setValue(<#T##value: Any?##Any?#>)
+        if let email = Auth.auth().currentUser?.email {
+            if uberHasBeenCalled {
+                uberHasBeenCalled = false
+                callAnUberButton.setTitle("Call an Uber", for: .normal)
+                Database.database().reference().child("RideRequests").childByAutoId().queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapshot) in
+                    snapshot.ref.removeValue()
+                    Database.database().reference().child("RideRequests").removeAllObservers()
+                }
+            } else {
+                let rideRequestDictionary: [String:Any] = ["email":email, "lat":userLocation.latitude, "lon":userLocation.longitude]
+                Database.database().reference().child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
+                uberHasBeenCalled = true
+                callAnUberButton.setTitle("Cancel Uber", for: .normal)
+            }
+        }
     }
     
     @IBAction func logoutTapped(_ ender: Any) {
-        
+        try? Auth.auth().signOut()
+        navigationController?.dismiss(animated: true, completion: nil)
     }
     
 }
